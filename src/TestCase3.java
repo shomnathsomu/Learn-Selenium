2f4017ab1d64decc8fc6281ca5184601234855d9
shomnathsomu <shomnathcse22@gmail.com>
2023-01-18 10:08:48 +0600

    add TestCase3 java file

---
+++ src/TestCase3.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestCase3 {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.gecko.driver", "D:\\\\Webdrivers\\\\geckodriver-v0.32.0-win64\\\\geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		// Locator identifiers
+		// ID/ ClassName/ Name/ linkText/ Xpath/ Css
+		driver.findElement(By.id("email")).sendKeys("harrypotter@gmail.com");
+		driver.findElement(By.name("pass")).sendKeys("1234567890");
+		driver.findElement(By.linkText("Forgotten password?")).click();
+		
+		// Create an object of a class
+		//Method1 method1 = new Method1();
+		
+		// pass the parameters of the methods
+		//System.out.println(method1.sum(1000, 111));
+		
+		//driver.close();
+
+	}
+
+}
