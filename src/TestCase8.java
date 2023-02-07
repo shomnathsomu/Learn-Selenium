278498a36a609276d79700f4561c08696d7ff6e8
shomnathsomu <shomnathcse22@gmail.com>
2023-02-07 17:57:37 +0600

    Identifying objects with Text using Xpath locators

--- src/TestCase8.java
+++ src/TestCase8.java
@@ -18,6 +18,9 @@ public class TestCase8 {
 		// Traverse to the parent element using Xpath
 		driver.findElement(By.xpath("//div[@class='header-upper']/div/div/div[2]/nav/div[2]/ul/li[3]/parent::ul/li[1]")).click();
 		
+		// Identifying objects with Text using Xpath locators
+		driver.findElement(By.xpath("//*[text()='JOIN NOW']")).click();
+		
 	}
 
 }
