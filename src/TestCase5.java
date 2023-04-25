941aacf2eac1a90c6e72d812c24dd47dadc24b77
shomnathsomu <shomnathcse22@gmail.com>
2023-04-25 17:26:11 +0600

    added the chrome options

--- src/TestCase5.java
+++ src/TestCase5.java
@@ -1,13 +1,18 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestCase5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:\\Webdrivers\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new ChromeDriver();
+		
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops);
 		
 		driver.get("https://login.salesforce.com/");
 		
