7d43b3060bc1306f3f82555f55e6b749b94bca56
shomnathsomu <shomnathcse22@gmail.com>
2023-04-27 17:14:38 +0600

    automated the dynamic drop-down

---
+++ src/TestCase11.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestCase11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Webdrivers\\chromedriver_win32\\chromedriver.exe");
+		
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops);
+		
+//		driver.get("https://www.wego.com.bd/flights");
+//		
+//		driver.findElement(By.className("dvaNnCiBXb6dFdBLR8Re")).click();
+//		for(int i=0; i<8; i++) {
+//			driver.findElement(By.className("gvC_jym6919cQYQsoZjp")).click();
+//		}
+//		driver.findElement(By.xpath("//div[@class='rovA_SwrnFH2dnQRwDcv']/button")).click();
+		
+		// go to the website
+		driver.get("https://www.easemytrip.com/flights.html");
+		
+		// Handle the dynamic drop-down
+		// Click on the TRAVELLER & CLASS drop-down
+		driver.findElement(By.id("trvlr_colm")).click();
+		
+		// Select 9 adults
+		for (int i = 0; i < 8; i++) {
+			driver.findElement(By.xpath("//div[@id='field1']/button[2]")).click();
+		}
+		// Reduce 1 adult
+		driver.findElement(By.xpath("//div[@id='field1']/button[1]")).click();
+		
+		// Select 7 infants
+		for (int i = 0; i < 7; i++) {
+			driver.findElement(By.xpath("//div[@id='field3']/button[2]")).click();
+		}
+		
+		// Select the business class radio button
+		driver.findElement(By.id("lbBusiness")).click();
+		
+		// Confirm the TRAVELLERS & CLASS
+		driver.findElement(By.id("traveLer")).click();
+		
+		//driver.close();
+		
+	}
+
+}
