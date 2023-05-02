17f2eb75fb568a10f3e1729d7add1b16baf51083
shomnathsomu <shomnathcse22@gmail.com>
2023-05-02 19:06:52 +0600

    End to End Automation using all UI Elements with selenium

--- src/TestCase11.java
+++ src/TestCase11.java
@@ -26,27 +26,37 @@ public class TestCase11 {
 		
 		// Click on the from city drop-down
 		driver.findElement(By.id("frmcity")).click();
-		
 		Thread.sleep(2000);
-		
 		// Parent-Child relationship locator to Identify the objects Uniquely
 		driver.findElement(By.xpath("//div[@id='fromautoFill'] //span[@id='spn6']")).click();
+		// Click on the To city drop-down
+		driver.findElement(By.id("tocity")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='toautoFill'] //span[@id='spn2']")).click();
+		
+		driver.findElement(By.id("dvfarecal")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='dvcalendar']/div/div[1]/div/div[3]/ul/li[3]")).click();
 		
 		// Handle the latest drop-down looping UI
 		// Click on the TRAVELLER & CLASS drop-down
 		driver.findElement(By.id("trvlr_colm")).click();
-		
-		// Select 9 adults
-		for (int i = 0; i < 8; i++) {
+		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+		// Select 4 adults
+		for (int i = 0; i < 3; i++) {
 			driver.findElement(By.xpath("//div[@id='field1']/button[2]")).click();
 		}
 		// Reduce 1 adult
 		driver.findElement(By.xpath("//div[@id='field1']/button[1]")).click();
 		
-		// Select 7 infants
-		for (int i = 0; i < 7; i++) {
+		// Select 2 infants
+		for (int i = 0; i < 4; i++) {
 			driver.findElement(By.xpath("//div[@id='field3']/button[2]")).click();
 		}
+		
+		// Handling Java alerts
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
 		
 		// Select the business class radio button
 		driver.findElement(By.id("lbBusiness")).click();
@@ -54,8 +64,10 @@ public class TestCase11 {
 		// Confirm the TRAVELLERS & CLASS
 		driver.findElement(By.id("traveLer")).click();
 		
+		driver.findElement(By.xpath("//button[@class='srchBtnSe']")).click();
+		
 		// Print the traveler number with text
-		System.out.println(driver.findElement(By.id("ptravlrNo")).getText());
+		//System.out.println(driver.findElement(By.id("ptravlrNo")).getText());
 		
 		//driver.close();
 		
