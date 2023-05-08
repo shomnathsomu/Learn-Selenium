3bfc57209cf771365cb8a2d63aaedfa100e1cac1
shomnathsomu <shomnathcse22@gmail.com>
2023-05-08 17:28:17 +0600

    added the Annotation helper attributes

--- src/myTestSuite/Day3.java
+++ src/myTestSuite/Day3.java
@@ -1,16 +1,36 @@
 package myTestSuite;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Day3 {
 	
+	@Test(timeOut = 10000)
+	public void MobileTest() {
+		System.out.println("Day 3: This is MobileTest method!");
+	}
+	
+	@Parameters({ "AmazonHome", "Login/Username" })
 	@Test
-	public void GroupTest1() {
-		System.out.println("This is GroupTest1 method! on Day3");
+	public void UrlTest(String urlName, String userName) {
+		System.out.println("Day 3: This is UrlTest method!");
+		System.out.println("Website: " + urlName);
+		System.out.println("Username: " + userName);
+	}
+	
+	// enabled is used to skip a test case intentionally
+	@Test(enabled = false)
+	public void AutomationTest() {
+		System.out.println("Day 3: This is MobileTest method!");
 	}
 	
 	@Test(groups = {"Regression"})
-	public void GroupTest2() {
-		System.out.println("This is GroupTest2 method! on Day3");
+	public void LaptopTest() {
+		System.out.println("Day 3: This is LaptopTest method!");
+	}
+	
+	@Test(dependsOnMethods = {"MobileTest"})
+	public void ApiLogin() {
+		System.out.println("Day 3: This is ApiLogin method!");
 	}
 }
