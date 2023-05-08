4b53d4f2b4dc873fef2b06bf5fdc9cca53297496
shomnathsomu <shomnathcse22@gmail.com>
2023-05-08 17:27:44 +0600

    added the Parameters

--- src/myTestSuite/Day2.java
+++ src/myTestSuite/Day2.java
@@ -5,42 +5,44 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Day2 {
 
 	@AfterTest
 	public void Flight() {
-		System.out.println("I will execute last in flight method!");
+		System.out.println("Day 2: I will execute last in flight method!");
 	}
 	
+	@Parameters({ "Trip" })
 	@Test
-	public void Hotel() {
-		System.out.println("This is hotel method!");
+	public void Resort(String urlName) {
+		System.out.println("Day 2: This is Resort method!");
+		System.out.println(urlName);
 	}
 	
 	@BeforeTest
 	public void HotelBooking() {
-		System.out.println("==========Day2============");
-		System.out.println("I will execute first in hotel booking method!");
+		System.out.println("Day 2: I will execute first in hotel booking method!");
 	}
 	
 	@Test(groups = {"Regression"})
 	public void Train() {
-		System.out.println("This is Train method on Day2!");
+		System.out.println("Day 2: This is Train method on Day2!");
 	}
 	
 	@BeforeMethod
 	public void Prerequisite() {
-		System.out.println("I will execute before every method");
+		System.out.println("Day 2: I will execute before every method");
 	}
 	
 	@AfterMethod
 	public void Conclusion() {
-		System.out.println("I will execute after every method");
+		System.out.println("Day 2: I will execute after every method");
 	}
 	@BeforeSuite
 	public void PrefaceSuite() {
-		System.out.println("Hello from BeforeSuite annotation class day2.");
+		System.out.println("Day 2: Hello from BeforeSuite annotation class day2.");
 	}
 }
