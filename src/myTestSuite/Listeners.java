798fe434ed428146c727e9b2effab020bda8ed98
shomnathsomu <shomnathcse22@gmail.com>
2023-05-09 17:27:23 +0600

    added all the required functions

---
+++ src/myTestSuite/Listeners.java
@@ -0,0 +1,43 @@
+package myTestSuite;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		
+	}
+	
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("The test case has been passed!");
+	}
+	
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("The test case has been failed! The test case: " + result.getName());
+	}
+	
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		
+	}
+	
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		
+	}
+	
+	@Override
+	public void onStart(ITestContext context) {
+		
+	}
+	
+	@Override
+	public void onFinish(ITestContext context) {
+		
+	}
+}
